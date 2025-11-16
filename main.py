2914bf24d3fc4420852ba82b53e97ca8503a057d
zzzpz <1911293650@qq.com>
2025-11-16 11:42:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-# 在这个文件中编写代码实现题目要求的功能
-import keyword  # 建议使用这个库处理关键字
-reserved_words = set(keyword.kwlist)
+import keyword
 
-# 以下内容自行完成
+with open('random_int.py', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+processed_lines = []
+for line in lines:
+    words = line.split()
+    new_words = []
+    for word in words:
+        if keyword.iskeyword(word):
+            new_words.append(word)
+        else:
+            new_words.append(word.upper())
+    processed_line = ' '.join(new_words) + '\n'
+    processed_lines.append(processed_line)
+
+with open('random_int_converted.py', 'w', encoding='utf-8') as f:
+    f.writelines(processed_lines)
