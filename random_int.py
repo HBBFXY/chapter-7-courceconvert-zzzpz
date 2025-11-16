a37ad3af8b2014c8bf6fe1068862dcd8f8da7ba7
zzzpz <1911293650@qq.com>
2025-11-16 11:49:16 +0800

    Update random_int.py

--- random_int.py
+++ random_int.py
@@ -1,8 +1,19 @@
-# 这个文件是要转换大小写的文件
-import random
-ls=[]
-random.seed(10)
-for i in range(10):
-    a=random.randint(0,100)
-    ls.append(a)
-print(ls)
+import keyword
+
+with open('random_int.py', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+processed_lines = []
+for line in lines:
+    words = line.split()
+    new_words = []
+    for word in words:
+        if keyword.iskeyword(word):
+            new_words.append(word)
+        else:
+            new_words.append(word.upper())
+    processed_line = ' '.join(new_words) + '\n'
+    processed_lines.append(processed_line)
+
+with open('random_int_converted.py', 'w', encoding='utf-8') as f:
+    f.writelines(processed_lines)
